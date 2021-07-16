34325c26cb823bebca2f6416e305074d0f4b41d4
acroscopic <52017620+acroscopic@users.noreply.github.com>
2021-07-16 08:42:41 -0500

    Update convertmusic.sh

--- convertmusic.sh
+++ convertmusic.sh
@@ -1,1 +1,24 @@
-asdf
+#!/bin/bash
+#script to covert audio files
+
+echo 'what filetype are you converting from'
+echo 'example: mp3, mp4, ogg, wav, webm...'
+echo '-------------------------------------'
+read OLD
+echo 'what would like for the new file extension?'
+echo '-------------------------------------------'
+read NEW
+echo 'what directory are the audio files in?'
+echo 'example: /home/user/Desktop/Music'
+echo '--------------------------------------'
+read DIR
+clear
+
+for i in $DIR/*.$OLD;
+	do NAME=`echo "$i" | cut -d'.' -f1`
+	echo "$NAME"
+	ffmpeg -i $i "${NAME}.$NEW"
+done
+
+#CAUTION: will destory all files in the directory you listed
+#rm -f $DIR/*.mp3
